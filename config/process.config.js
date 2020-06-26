7f7e796a6f6d1baf81007abaa2b9f630e497dbd0
Abijit Biswas <abhijitb.2204@gmail.com>
2020-06-26 19:43:44 +0530

    process log

--- config/process.config.js
+++ config/process.config.js
@@ -18,8 +18,8 @@ for ( p in allProcesses ) {
     processes.push({
         name            : p,
         script          : `${ __dirname }/../processes/${ p }.js`,
-        error_file      : `${ PM2_LOG_PATH }/${ new Date() }-${ p }-err.log`,
-        out_file        : `${ PM2_LOG_PATH }/${ new Date() }-${ p }-out.log`,
+        error_file      : `${ PM2_LOG_PATH }/${ new Date().toDateString() }-${ new Date().toLocaleTimeString() }-${ p }-err.log`,
+        out_file        : `${ PM2_LOG_PATH }/${ new Date().toDateString() }-${ new Date().toLocaleTimeString() }-${ p }-out.log`,
         watch           : false,
         env_development : {
             NODE_ENV       : "development"
@@ -35,11 +35,10 @@ for ( p in allProcesses ) {
 processes.push({
     name            : "API_PROCESS",
     script          : `${ __dirname }/../server.js`,
-    error_file      : `${ PM2_LOG_PATH }/${ new Date() }-API_PROCESS-err.log`,
-    out_file        : `${ PM2_LOG_PATH }/${ new Date() }-API_PROCESS-out.log`,
+    error_file      : `${ PM2_LOG_PATH }/${ new Date().toDateString() }-${ new Date().toLocaleTimeString() }-API_PROCESS-err.log`,
+    out_file        : `${ PM2_LOG_PATH }/${ new Date().toDateString() }-${ new Date().toLocaleTimeString() }-API_PROCESS-out.log`,
     watch           : false,
     env_development : {
-        DEBUG          : "API_PROCESS:*",
         NODE_ENV       : "development"
     },
     env_production  : {
